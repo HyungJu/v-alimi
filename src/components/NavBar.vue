117388431b35c0c6193cfe4476406feb21becd3d
HyungJu <sungkisa@naver.com>
2020-09-10 08:38:22 +0900

    feat: create NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav>
+        <div class="wrapper">
+            <div class="brand">
+                <p>E알리미</p>
+            </div>
+            <div class="right">
+                <div class="select">
+                    받은 알리미
+                </div>
+                <div class="select selected">
+                    학급 알리미
+                </div>
+                <div class="select">
+                    도움말
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    nav{
+        display: flex;
+        width: 100vw;
+        height: 60px;
+        background-color: #1C1C24;
+        border-bottom: 2px solid #3b3b4b;
+        justify-content: center;
+    }
+    .wrapper{
+        width: 95vw;
+        max-width: 1024px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .brand{
+        p{
+            font-family: GmarketSansBold,sans-serif;
+            font-weight: bold;
+            color: white;
+            font-size: 1rem;
+        }
+    }
+    .select{
+        color: #5F5F6E;
+        font-family: GmarketSansMedium,sans-serif;
+        font-weight: normal;
+        margin-left: 15px;
+    }
+    .selected{
+        color: white;
+    }
+    .right{
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
