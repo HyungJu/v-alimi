09039d246340991b4e82ea4d5dcecee497488ff8
HyungJu <sungkisa@naver.com>
2020-09-10 11:05:24 +0900

    feat: create AlimiCard

---
+++ src/components/AlimiCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'AlimiCard',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
