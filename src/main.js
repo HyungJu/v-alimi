59757fb2f296a67a3e0a6f7955f3265ef712efb9
HyungJu <sungkisa@naver.com>
2020-09-10 19:45:46 +0900

    chore: add and configure vue-tailwind

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/custom.scss'
+import VueTailwind from 'vue-tailwind'
+import settings from '@/assets/theme.js'
 
 Vue.config.productionTip = false
-
+Vue.use(VueTailwind, settings)
 new Vue({
   router,
   store,
